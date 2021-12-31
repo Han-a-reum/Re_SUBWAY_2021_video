b2faddb4b76ea7feee1dfd890bd7631e6dfe7b59
Han-a-reum <hanar0323@naver.com>
2021-12-31 22:48:22 +0900

    ddsadsd라!!

--- index.ssg.php
+++ index.ssg.php
@@ -10,10 +10,10 @@ $pageCode = "home";
       <img src="https://github.com/Han-a-reum/Re_SUBWAY_2021_video/blob/master/resource/img/bn_1_1.png?raw=true" alt="">
     </div>
     <div class="box_1_2">
-    <a href="pf.ssg.php"><img src="./resource/img/bn_1_2.png" alt=""></a>
+    <a href="pf.ssg.php"><img src="https://github.com/Han-a-reum/Re_SUBWAY_2021_video/blob/master/resource/img/bn_1_2.png?raw=true" alt=""></a>
     </div>
     <div class="box_1_4 chatbox">
-    <a href="pf.ssg.php"><img src="./resource/img/bn_1_4.png" alt=""></a>
+    <a href="pf.ssg.php"><img src="https://github.com/Han-a-reum/Re_SUBWAY_2021_video/blob/master/resource/img/bn_1_4.png?raw=true" alt=""></a>
     </div>
   </section>
 
--- pf.ssg.php
+++ pf.ssg.php
@@ -7,7 +7,7 @@ $pageCode = "home";
 
 <section class="section-main-1">
     <div class="box_1_1">
-      <img src="./resource/img/bn_2_1.png" alt="">
+      <img src="https://github.com/Han-a-reum/Re_SUBWAY_2021_video/blob/master/resource/img/bn_2_1.png?raw=true" alt="">
     </div>
     <div class="box_3_1">
     <video oncontextmenu="return false;" id="myVideo" width="1256" controls>
